c4ac6cf36df8b5204654aa7361df3e71c7b15db5
Gutsover <mylenevandaele@hmail.com>
2018-05-15 23:01:10 +0100

    Add lorem

--- src/routes/realisations/index.js
+++ src/routes/realisations/index.js
@@ -1,6 +1,21 @@
 import { h } from "preact";
 import style from "./style";
 
-const Realisations = () => <div />;
+const Realisations = () => (
+  <main>
+    <h1>REA</h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in massa at nibh mattis imperdiet a nec ipsum. Morbi a erat quis metus consectetur aliquet vitae vel sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum laoreet faucibus viverra. Morbi ultricies, tortor a iaculis gravida, justo arcu dignissim enim, sed mattis purus urna a enim. Duis ut blandit felis, volutpat vestibulum est. Nunc imperdiet sem a turpis fringilla mollis. Sed porttitor nibh velit, eget porta nibh faucibus id. Nunc pretium blandit cursus. Vestibulum facilisis, purus quis auctor tincidunt, lacus enim lacinia ipsum, tincidunt posuere tellus dolor ac ex. Maecenas sollicitudin volutpat velit scelerisque vehicula.
+
+Sed hendrerit fringilla ultricies. Nulla pharetra eros in tempus tristique. Vestibulum posuere risus vitae nisi congue, sit amet rutrum diam tincidunt. Duis in purus scelerisque lacus tincidunt dapibus in vel nulla. Proin dolor nisi, iaculis sed iaculis sit amet, vehicula at augue. Phasellus ut nunc at augue semper posuere eget laoreet purus. Nunc fringilla ante quis felis tempus, sed efficitur sapien finibus.
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris volutpat, diam eget sagittis laoreet, ante ipsum finibus lacus, et hendrerit enim orci ac enim. Aliquam sed facilisis lectus. Phasellus ex tortor, tempus quis tempor ac, tincidunt ut lacus. Vivamus sed aliquam sapien. Integer sapien lorem, ullamcorper vel magna ac, vestibulum hendrerit quam. Nam viverra leo id eros semper, a efficitur dui imperdiet.
+
+Quisque eleifend hendrerit sodales. Nam luctus justo vel mauris tristique sodales. Nulla egestas turpis ac mauris vestibulum placerat. Aliquam mattis justo eget malesuada efficitur. Aenean sollicitudin turpis id magna congue pharetra. Pellentesque sodales, mi ac feugiat tempor, sapien nunc mattis nunc, eu dignissim massa mauris at ligula. Morbi augue libero, dictum ac fermentum nec, bibendum ac tellus. Cras molestie, velit non eleifend ullamcorper, ligula nisl suscipit sapien, feugiat sodales erat lacus quis odio. Suspendisse potenti. Mauris vel malesuada tellus, vitae iaculis neque.
+
+Donec pretium placerat libero. Etiam lobortis consectetur nunc, ut rutrum quam facilisis vitae. Vestibulum pulvinar rhoncus sollicitudin. Nam congue interdum faucibus. Nullam et nunc metus. Praesent faucibus varius tellus, at lobortis augue ultricies sit amet. Ut convallis, felis quis accumsan lacinia, nunc velit feugiat nulla, ut cursus libero libero sit amet magna. Phasellus quis magna eget sem laoreet euismod a vitae ante. Donec sagittis bibendum metus, non lobortis ipsum gravida nec. Nulla facilisi.
+    </p>
+  </main>
+);
 
 export default Realisations;

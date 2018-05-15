afb08d42bcea2d616db7384be1c7b2039910d0fc
Gutsover <mylenevandaele@hmail.com>
2018-05-15 19:22:00 +0100

    Add Menu animations

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,20 +5,22 @@ import style from "./style";
 class Menu extends preact.Component {
   render() {
     return (
-      <nav class={style.menu}>
-        <Link activeClassName={style.active} class={style.menu__link} href="/">
-          accueil
-        </Link>
-        <Link activeClassName={style.active} class={style.menu__link} href="/competences">
-          compétences
-        </Link>
-        <Link activeClassName={style.active} class={style.menu__link} href="/realisations">
-          réalisations
-        </Link>
-        <Link activeClassName={style.active} class={style.menu__link} href="/contact">
-          contact
-        </Link>
-      </nav>
+      <div class={style["menu-container"]}>
+        <nav class={style.menu}>
+          <Link activeClassName={style.active} class={style.menu__link} href="/">
+            accueil
+          </Link>
+          <Link activeClassName={style.active} class={style.menu__link} href="/competences">
+            compétences
+          </Link>
+          <Link activeClassName={style.active} class={style.menu__link} href="/realisations">
+            réalisations
+          </Link>
+          <Link activeClassName={style.active} class={style.menu__link} href="/contact">
+            contact
+          </Link>
+        </nav>
+      </div>
     );
   }
 }

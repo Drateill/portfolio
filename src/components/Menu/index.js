cef26667cc69850295328bef21efba6486936c56
Gutsover <mylenevandaele@hmail.com>
2018-05-15 20:10:52 +0100

    refactor

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,27 +2,23 @@ import preact from "preact";
 import { Link } from "preact-router/match";
 import style from "./style";
 
-class Menu extends preact.Component {
-  render() {
-    return (
-      <div class={style["menu-container"]}>
-        <nav class={style.menu}>
-          <Link activeClassName={style.active} class={style.menu__link} href="/">
-            accueil
-          </Link>
-          <Link activeClassName={style.active} class={style.menu__link} href="/competences">
-            compétences
-          </Link>
-          <Link activeClassName={style.active} class={style.menu__link} href="/realisations">
-            réalisations
-          </Link>
-          <Link activeClassName={style.active} class={style.menu__link} href="/contact">
-            contact
-          </Link>
-        </nav>
-      </div>
-    );
-  }
-}
+const Menu = () => (
+  <div class={style["menu-container"]}>
+    <nav class={style.menu}>
+      <Link activeClassName={style.active} class={style.menu__link} href="/">
+        accueil
+      </Link>
+      <Link activeClassName={style.active} class={style.menu__link} href="/competences">
+        compétences
+      </Link>
+      <Link activeClassName={style.active} class={style.menu__link} href="/realisations">
+        réalisations
+      </Link>
+      <Link activeClassName={style.active} class={style.menu__link} href="/contact">
+        contact
+      </Link>
+    </nav>
+  </div>
+);
 
 export default Menu;

08cdb912e70ecbd59df82580ba0cb4e068e0dfba
Gutsover <mylenevandaele@hmail.com>
2018-05-16 16:26:21 +0100

    Add contenu page home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,14 +7,26 @@ const Home = () => (
       <div class="col-12 text-center">
         <h1><span>M</span>ylène <span>V</span>andaële</h1>
       </div>
-
-      <div class="col-6">
+      
+      <div class={style.photo + " col-sm-6 col-md-4 offset-md-2"} >
         <img class={style.photo + " img-fluid"} src="../assets/img/photo-nb.jpg" />
       </div>
+      
+      <div class={style.title + " col-sm-6 col-md-4"}>
+        <h2 class="align-middle">Développeuse<br /> intégratrice<br />front-end</h2>
+      </div>
 
-      <div class="col-6">
-        <h2>Développeuse <br /> intégratrice front-end</h2>
-        <p>Moi je ne crois pas qu'il y ait de bonne ou de mauvaise situations, mais si je devais résumer ma vie avec vous, je dirais que c'est d'abord des rencontres!</p>
+      
+      <div class="col-12 text-center">
+        <p>lorem ipsum</p>
+      </div>
+
+      <div class="col-sm-6 col-md-3 offset-md-3">
+        <button type="button" class="btn btn-dark">Me contacter</button>
+      </div>
+
+      <div class="col-sm-6 col-md-3">
+        <button type="button" class="btn btn-dark">Qui suis-je ?</button>
       </div>
 
     </div>

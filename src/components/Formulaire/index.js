0a1841e539dfe74b25a1a75d974655a1d8e9bcda
Gutsover <mylenevandaele@hmail.com>
2018-05-18 18:35:34 +0100

    Remove bootstrap

--- src/components/Formulaire/index.js
+++ src/components/Formulaire/index.js
@@ -3,19 +3,15 @@ import { Link } from "preact-router/match";
 import style from "./style";
 
 const Formulaire = () => (
-  
   <form class={style.form}>
+    <input type="text" class={style.name} placeholder="Votre Nom" />
 
-      <input type="text" class={style.name} placeholder="Votre Nom" />
+    <input type="email" class={style.mail} placeholder="Votre adresse E-Mail" />  
 
-      <input type="email" class={style.mail} placeholder="Votre adresse E-Mail" />  
-
-      <textarea class={style.message} rows="3" placeholder=" Vous : 'Vous sembez tellement douée et fantaisiste que je souhaite vous prendre dans mon équipe de super dev!'"></textarea>
-      
-      <button type="button" class={"btn btn-lg btn-block " + style.button}>Envoyez votre message</button>
-  
+    <textarea class={style.message} rows="3" placeholder=" Vous : 'Vous sembez tellement douée et fantaisiste que je souhaite vous prendre dans mon équipe de super dev!'"></textarea>
+    
+    <button type="button" class={style.button}>Envoyez votre message</button>
   </form>
- 
 );
 
 export default Formulaire;
--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -5,11 +5,7 @@ import Formulaire from "../../components/Formulaire";
 
 const Contact = () => (
   <main>
-    <div class="row">
-      <div class="col-sm-12 col-md-6 offset-md-3">
-        <Formulaire />
-      </div>
-    </div>
+    <Formulaire />
   </main>
 
 );
